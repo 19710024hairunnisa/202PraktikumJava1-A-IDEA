ed22e7cfe2e465d80efc142f405aa29ffc647c82
Your Name <19710024hairunnisa@gmail.com>
2021-06-09 20:25:14 +0800

    Membuat Objek Kedua dan Menampilkan Nilai Atribut

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -8,5 +8,19 @@ public class MahasiswaBeraksi {
         mahasiswa.npm = "19639999";
         mahasiswa.nama = "Brian";
         mahasiswa.tanggalLahir = new Date();
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggalLahir);
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "19638888";
+        mahasiswa2.nama = "Tiffany";
+        mahasiswa2.tanggalLahir = new Date();
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
     }
+
 }
