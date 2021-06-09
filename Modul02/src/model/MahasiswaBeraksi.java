02439e30bf4c83fcae1a61f69508e409c951e2f9
Your Name <19710024hairunnisa@gmail.com>
2021-06-09 20:30:46 +0800

    Menambahkan tipe data Date dengan menggunakan SimpleDateFormat

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] arg) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19639999";
         mahasiswa.nama = "Brian";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("09-12-1999");
 
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa.npm);
@@ -16,7 +22,7 @@ public class MahasiswaBeraksi {
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19638888";
         mahasiswa2.nama = "Tiffany";
-        mahasiswa2.tanggalLahir = new Date();
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("10-08-2001");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
