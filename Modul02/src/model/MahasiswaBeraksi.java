8403775cb4a7e0694db0a31b99394eb9b5f9742a
Your Name <19710024hairunnisa@gmail.com>
2021-06-09 20:48:12 +0800

    Menambah void Menyapa dan menambahkan data pada MahasiswaBeraksi

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -27,6 +27,10 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
         System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+
+        mahasiswa.tampilkanAtribut();
+        mahasiswa.menyapa();
+        Mahasiswa mahasiswa3 = new Mahasiswa();
     }
 
 }
